503bc6688907dbf1bf7e2448034e7ef239dd9fb1
franciowel <spina_francesco@blu.it>
2022-06-23 15:36:39 +0200

    inserito h1

---
+++ script.js
@@ -0,0 +1,11 @@
+Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue(
+    {
+        el: '#root',
+        data : {
+            title : 'hello VueJS'
+        }
+    }
+)
